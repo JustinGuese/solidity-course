215ba3092eb99ccb6768e85d34a24a0848b7a0df
Justin Guese <guese.justin@gmail.com>
2021-10-18 17:21:22 +0200

    adaptions for tether deployment ^.8

---
+++ defi-staking-app-starter-main/defi-staking-app-starter/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8;
+
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8 public decimals = 18;
+}
