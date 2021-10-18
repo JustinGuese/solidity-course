42dced148bb1d7b2e40b99651870b0704f9b1026
Justin Guese <guese.justin@gmail.com>
2021-10-18 17:32:18 +0200

    tether update

--- defi-staking-app-starter-main/defi-staking-app-starter/contracts/Tether.sol
+++ defi-staking-app-starter-main/defi-staking-app-starter/contracts/Tether.sol
@@ -5,4 +5,30 @@ contract Tether {
     string public symbol = "USDT";
     uint public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "require at least x ");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
 }
