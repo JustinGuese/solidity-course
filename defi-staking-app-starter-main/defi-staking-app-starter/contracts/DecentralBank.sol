96fa1c97778acf2eb3ac5c58bfb9a5c577e33351
Justin Guese <guese.justin@gmail.com>
2021-10-18 19:47:07 +0200

    migration script tested

--- defi-staking-app-starter-main/defi-staking-app-starter/contracts/DecentralBank.sol
+++ defi-staking-app-starter-main/defi-staking-app-starter/contracts/DecentralBank.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.8;
 
+import "./REW.sol";
+import "./Tether.sol";
+
 contract DecentralBank {
     address public owner;
     string public name = "Justins Bank";
-
     
+    Tether public tether;
+    REW public rwd;
+
+    constructor(REW _rew, Tether _tether) {
+        rwd = _rew;
+        tether = _tether;
+    }
 }
--- defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,8 +2,17 @@ const Tether = artifacts.require("Tether");
 const REW = artifacts.require("REW");
 const DB = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
   await deployer.deploy(REW);
-  await deployer.deploy(DB);
+  const rwd = await REW.deployed();
+
+  await deployer.deploy(DB, rwd.address, tether.address);
+  const db = await DB.deployed();
+  
+  await rwd.transfer(db.address, "1000000000000000000000000");
+
+  // distribute 100 tether to investor
+  await tether.transfer(accounts[1], "1000000000000000000");
 };
