b0381fe15ca96258913ae888c835219fd4a6b7ce
Justin Guese <guese.justin@gmail.com>
2021-10-19 19:44:27 +0200

    staking test

--- kingdom/test/decentralBank.test.js
+++ kingdom/test/decentralBank.test.js
@@ -97,14 +97,55 @@ contract("KingdomBank", (accounts) => {
             let ethbal_prev = await web3.eth.getBalance(accounts[1]);
             ethbal_prev_expected = ethbal_prev - new web3.utils.BN(web3.utils.toWei("1", "ether"));
             ethbal_prev_expected = Number(web3.utils.fromWei(ethbal_prev_expected.toString()));
-            console.log("ethbal current",ethbal_prev ,"ethbal_prev_expected: ", ethbal_prev_expected);
+            // console.log("ethbal current",ethbal_prev ,"ethbal_prev_expected: ", ethbal_prev_expected);
             // await fails somehow
             res = await kb.buyForETH({from: accounts[1], value: new web3.utils.BN(web3.utils.toWei("1", "ether"))});
             let ethbal = await web3.eth.getBalance(accounts[1]);
             ethbal = Number(web3.utils.fromWei(ethbal.toString()));
             // little less bc gas fee
             ethbal.should.lessThan(ethbal_prev_expected);
+
+            // now check balance of kgdsc
+            let balance = await kgdsc.balanceOf(accounts[1]);
+            balance = balance.toString();
+            // 100 bc our multiplier for purchased eth is 100
+            balance.should.equal(web3.utils.toWei("100", "ether"));
         });
 
+        it("should be possible to stake these bought coins for attackcoins", async() => {
+            let prev_balance = await kgdsc.balanceOf(accounts[1]);
+            prev_balance = prev_balance.toString();
+            // stake 75% of seecoins
+            // first we need to allow the contract to transfer the tokens
+            await kgdsc.approve(kb.address, web3.utils.toWei("100", "ether"), {from: accounts[1]});
+
+            let res = await kb.plantForAttackpoints(web3.utils.toWei("75", "ether"), {from: accounts[1]});
+            let balance = await kgdsc.balanceOf(accounts[1]);
+            balance = balance.toString();
+            // 75% of seecoins should be staked
+            balance.should.equal(web3.utils.toWei("25", "ether"));
+        });
+
+        it("should not be possible to unstake before time if over", async() => {
+            let res = await kb.harvestAll({from: accounts[1]});
+            res = await kb.getCurrentStakes({from: accounts[1]});
+            let attackPoints = res[0].toString();
+            let defensePoints = res[1].toString();
+            attackPoints.should.equal(web3.utils.toWei("75", "ether"));
+            defensePoints.should.equal("0");
+            console.log("in staking: attackpoints/defensepoints", attackPoints, defensePoints);
+            // and finally check if balance is still correct
+            let balance = await kgdsc.balanceOf(accounts[1]);
+            balance = balance.toString();
+            balance.should.equal(web3.utils.toWei("25", "ether"));
+            // also if attackpoints are not prematurely harvested
+            balance = await kgdat.balanceOf(accounts[1]);
+            balance = balance.toString();
+            balance.should.equal("0");
+            // same for def points
+            balance = await kgddf.balanceOf(accounts[1]);
+            balance = balance.toString();
+            balance.should.equal("0");
+        });
     });
 });
